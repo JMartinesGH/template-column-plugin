5a91853597d6da4da5c3a0ccb090083dd6ac17af
Jamie Martines <dev.jpm@gmail.com>
2021-06-03 11:34:51 -0400

    Using get page_templates() to return array of all templates available to the theme, loop through each template and if valid return name

--- template-column-plugin.php
+++ template-column-plugin.php
@@ -9,15 +9,28 @@
 
  # create Template Name column on pages view
 add_filter('manage_pages_columns', 'template_name_pages_column');
-function template_name_pages_column($columns){
+function template_name_pages_column( $columns ){
     #adds Template Name as 'template-name'
     $columns['template-name'] = __('Template Name', 'template-name');
     return $columns;
 }
 
 add_action('manage_pages_custom_column', 'template_name_column', 10,2);
-function template_name_column($column_name, $id){
+function template_name_column( $column_name, $id ){
+    # if column template name exists
     if ( 'template-name' === $column_name ) {
-        echo get_post_meta( $id, '_wp_page_template', true );
+        // echo get_post_meta( $id, '_wp_page_template', true );
+        $template_data = get_post_meta( $id, '_wp_page_template', true );
+        if ( 'default' == $template_data ) {
+            # echo the default page template name
+            echo __('Default Template', 'template-name');
+        }
+        # get all templates available in theme
+        $templates = get_page_templates();
+        # loop through the keys in templates array
+        foreach ( array_keys( $templates ) as $template ) {
+            # if template data matches templates[template] return template name
+            if ( $template_data == $templates[$template] ) echo $template;
+        }
     }
 }

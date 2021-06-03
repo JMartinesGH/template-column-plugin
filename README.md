d870f189581d3a38c2d51c57ab6a5203ef11fd67
Jamie Martines <dev.jpm@gmail.com>
2021-06-03 11:01:48 -0400

    update README.md with installation instructions, add vscode settings, create entry php file - template-column-plugin.php

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # template-column-plugin
+
+## About
+WP Plugin that adds page template column to the pages view in WP-Admin
+
+## Installation
+
+1. Clone the git repository into your plugins directory
+2. Activate the plugin in `/wp-admin/plugins.php`
+
+
---
+++ template-column-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Template Column Plugin
+ * Plugin URI: https://github.com/JMartinesGH/template-column-plugin
+ * Description: Plugin that adds page template column to the pages view in WP-Admin
+ * Author: Jamie Martines
+ * Version: 0.0.1
+ */
+?>

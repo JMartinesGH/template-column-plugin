4c2ad705898aeab72dffd668faff36342f9ac50f
Jamie Martines <dev.jpm@gmail.com>
2021-06-03 11:21:32 -0400

    Added template slug to column. Need to get template name string still

--- template-column-plugin.php
+++ template-column-plugin.php
@@ -7,8 +7,17 @@
  * Version: 0.0.1
  */
 
+ # create Template Name column on pages view
 add_filter('manage_pages_columns', 'template_name_pages_column');
 function template_name_pages_column($columns){
-    $columns['template-name'] = __('Template Name');
+    #adds Template Name as 'template-name'
+    $columns['template-name'] = __('Template Name', 'template-name');
     return $columns;
+}
+
+add_action('manage_pages_custom_column', 'template_name_column', 10,2);
+function template_name_column($column_name, $id){
+    if ( 'template-name' === $column_name ) {
+        echo get_post_meta( $id, '_wp_page_template', true );
+    }
 }
